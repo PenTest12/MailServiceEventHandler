ff055d5696be7353006bcb72b90d8c3d0abfc7cb
justsayantan <justsayantan@gmail.com>
2016-04-03 12:21:59 +0530

    Added the Code

---
+++ com.events.MailServiceEventHandler/com.events.MailServiceEventHandler/MailServiceEventHandler.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Web;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Xml.Linq;
+using Tridion.ContentManager;
+using Tridion.ContentManager.ContentManagement;
+using Tridion.ContentManager.CommunicationManagement;
+using Tridion.ContentManager.ContentManagement.Fields;
+using Tridion.ContentManager.Extensibility;
+using Tridion.ContentManager.Extensibility.Events;
+using Tridion.ContentManager.Publishing;
+using Tridion.Logging;
+using System.ServiceModel;
+using System.Net.Mail;
+
+namespace com.events.MailServiceEventHandler
+{
+    [TcmExtension("MailServiceEventHandlerEventSystemExtension")]
+    class MailServiceEventHandler : TcmExtension
+    {
+        public MailServiceEventHandler()
+        {
+            Subscribe();
+        }
+
+        public void Subscribe()
+        {
+
+            EventSystem.Subscribe<Component, SaveEventArgs>(SentMailOnComponentPublish, EventPhases.Processed);
+        }
+
+        /// <summary>
+        /// This method will update the meta data content of a component (Using Product schema).
+        /// </summary>
+        /// <param name="subject"></param>
+        /// <param name="args"></param>
+        /// <param name="phase"></param>
+        private void SentMailOnComponentPublish(Component component, EventArgs args, EventPhases phase)
+        {
+
+            string schemaURI = component.Schema.Title.ToString();
+            Logger.Write(string.Format("SchemaURI-->  {0} ", schemaURI), "ComponentServiceEventHandlerEventSystemExtension", LoggingCategory.General, TraceEventType.Information);
+
+            string schemaName = Utility.GetSchemafromConfig();
+            Logger.Write(string.Format("SchemaName-->  {0} ", schemaName), "ComponentServiceEventHandlerEventSystemExtension", LoggingCategory.General, TraceEventType.Information);
+
+            if (schemaURI.Contains(schemaName))
+            {
+                TcmUri componentID = component.Id;
+                Logger.Write(string.Format("Component ID -->  {0} ", componentID), "ComponentServiceEventHandlerEventSystemExtension", LoggingCategory.General, TraceEventType.Information);
+
+                string componentTitle = component.Title.ToString();
+                Logger.Write(string.Format("Component Title -->  {0} ", componentTitle), "ComponentServiceEventHandlerEventSystemExtension", LoggingCategory.General, TraceEventType.Information);
+
+                SendMail();
+            }
+
+        }
+
+        private void SendMail()
+        {
+            MailMessage msg = Utility.WorkflowMailMessageConfiguration();
+            SmtpClient client = Utility.SMTPClientConfiguration();
+
+            try
+            {
+                client.Send(msg);
+                Logger.Write(string.Format("Mail Sent -->  {0} ", msg.Body.ToString()), "ComponentServiceEventHandlerEventSystemExtension", LoggingCategory.General, TraceEventType.Information);
+            }
+            catch (Exception ex)
+            {
+                Logger.Write(string.Format("Error -->  {0} ", ex.Message), "ComponentServiceEventHandlerEventSystemExtension", LoggingCategory.General, TraceEventType.Error);
+            }
+        }
+    }
+}
---
+++ com.events.MailServiceEventHandler/com.events.MailServiceEventHandler/Utility.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Net.Mail;
+using System.Net;
+using System.IO;
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace com.events.MailServiceEventHandler
+{
+        static class Utility
+        {
+            public static SmtpClient SMTPClientConfiguration()
+            {
+                AppSettingsSection section = GetAppSettings();
+
+                SmtpClient client = new SmtpClient();
+
+                //Get variable from 
+                string _MailServer = section.Settings["MailServer"].Value;
+                string _CredentialsID = section.Settings["CredentialsID"].Value;
+                string _CredentialsPassword = section.Settings["CredentialsPassword"].Value;
+
+                //Set value to smtp client
+                client.Host = _MailServer;
+                client.UseDefaultCredentials = false;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.Credentials = new NetworkCredential(_CredentialsID, _CredentialsPassword);
+
+                return client;
+            }
+
+            public static MailMessage WorkflowMailMessageConfiguration()
+            {
+
+                AppSettingsSection section = GetAppSettings();
+                /// <summary>
+                /// Get Mail Settings
+                /// </summary>
+                string _MessageBodyXslt = section.Settings["MessageBodyXslt"].Value;
+                string _MessageSubject = section.Settings["MessageSubject"].Value;
+                string _MailTo = section.Settings["MailTo"].Value;
+                string _MailFrom = section.Settings["MailFrom"].Value;
+
+                MailMessage msg = new MailMessage(_MailFrom, _MailTo);
+                msg.Body = GetHtmlBody(_MessageBodyXslt);
+                msg.IsBodyHtml = true;
+                msg.Subject = _MessageSubject;
+
+                return msg;
+            }
+
+            public static string GetSchemafromConfig()
+            {
+
+                AppSettingsSection section = Utility.GetAppSettings();
+                string schemaName = section.Settings["Schema"].Value;
+
+                return schemaName;
+            }
+
+            public static string GetHtmlBody(string messageBodyXslt)
+            {
+                string xmlInput = @"<?xml version=""1.0"" encoding=""utf-8"" ?>
+                                <EmailTemplate>
+                                <subject>Information from xyz</subject>
+                                <displayName>abcd</displayName>
+                                <Message1>
+                                    Thanks you for registering to xyz.
+                                </Message1>
+                                <Copyright>Copyright xyz</Copyright>
+                                </EmailTemplate>";
+
+                string xslInput = System.IO.File.ReadAllText(messageBodyXslt);
+
+                using (StringReader srt = new StringReader(xslInput)) // xslInput is a string that contains xsl
+                using (StringReader sri = new StringReader(xmlInput)) // xmlInput is a string that contains xml
+                {
+                    using (XmlReader xrt = XmlReader.Create(srt))
+                    using (XmlReader xri = XmlReader.Create(sri))
+                    {
+                        XslCompiledTransform xslt = new XslCompiledTransform();
+                        xslt.Load(xrt);
+                        using (StringWriter sw = new StringWriter())
+                        using (XmlWriter xwo = XmlWriter.Create(sw, xslt.OutputSettings)) // use OutputSettings of xsl, so it can be output as HTML
+                        {
+                            xslt.Transform(xri, xwo);
+                            return sw.ToString();
+                        }
+                    }
+                }
+            }
+
+            private static AppSettingsSection GetAppSettings()
+            {
+                System.Configuration.ExeConfigurationFileMap configFileMap = new ExeConfigurationFileMap();
+                configFileMap.ExeConfigFilename = @"<%Tridion Home%>\config\com.events.MailServiceEventHandler.dll.config";
+
+                System.Configuration.Configuration configuration = ConfigurationManager.OpenMappedExeConfiguration(configFileMap, ConfigurationUserLevel.None);
+                AppSettingsSection section = (AppSettingsSection)configuration.GetSection("appSettings");
+                return section;
+            }
+        }
+    
+}
